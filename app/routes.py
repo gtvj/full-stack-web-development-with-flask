1312fd4c5833fac79dce8e8fc537ace3a1911928
Gwyn Jones <m@gwnjns.com>
2019-11-08 14:30:14 +0000

    Extract <head> to include and add template variables

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,9 @@ from flask import render_template
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("index.html", new_user=False)
+    return render_template("index.html", new_user=False, title="Home page")
 
 
 @app.route('/thoughts')
 def thoughts():
-    return render_template("thoughts.html")
+    return render_template("thoughts.html", title="Thoughts page")
